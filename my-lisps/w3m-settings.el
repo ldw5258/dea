9e53bc258bf03bb88b26691b4d3c0fb7b430872b
L.D.W <ldw5258@126.com>
2011-08-16 01:08:03 +0800

    修改my-lisps/w3m-settings.el,添加快捷键C-x j，执行命令toggle-truncate-lines

--- my-lisps/w3m-settings.el
+++ my-lisps/w3m-settings.el
@@ -1,6 +1,6 @@
 ;; -*- Emacs-Lisp -*-
 
-;; Time-stamp: <2011-03-20 12:48:27 Sunday by taoshanwen>
+;; Time-stamp: <2011-08-16 00:59:35 Tuesday by ldw>
 
 (autoload 'w3m-which-command "w3m-util")
 
@@ -183,7 +183,8 @@
      ("r"         w3m-reload-this-page)
      ("v"         w3m-bookmark-view-new-session)
      ("M-e"       w3m-bookmark-edit)
-     ("'"         switch-to-other-buffer))))
+     ("'"         switch-to-other-buffer)
+     ("C-x j"       toggle-truncate-lines))))
 
 (eval-after-load "w3m"
   `(w3m-settings))
--- my.emacs
+++ my.emacs
@@ -2,3 +2,7 @@
 
 ;; 添加git支持
 (require 'git-emacs)
+
+;; all backups goto ~/.backups instead in the current directory
+(setq backup-directory-alist (quote (("." . "~/.backups"))))
+
